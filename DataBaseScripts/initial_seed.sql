463de79a17b04a7782ea0de98f05d522682787ff
Jorgiy <jorgiy@bk.ru>
2018-04-23 23:23:51 +0300

    Add DataBase scripts and main projects

---
+++ DataBaseScripts/initial_seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO [PRODUCTS]
+           ([NAME]
+           ,[IMG_URI]
+           ,[PRICE]
+           ,[DESCRIPTION])
+     VALUES
+           ('Mobile Phone', 'ourhost.static.com/img/products/eac756f.png',12548.5,'The best mobile phone, that you have ever seen'),
+		   ('Plazma TV', 'ourhost.static.com/img/products/6785ff1.png',45612.1,'Good enough plazma TV'),
+		   ('Tablet PC', 'ourhost.static.com/img/products/facb55d.png',19003,'Tablet PC for navigating the net and watching movies'),
+		   ('Laptop', 'ourhost.static.com/img/products/9870e76.png',30221.9,'Average laptop for every day working'),
+		   ('Stationare PC', 'ourhost.static.com/img/products/abf9eed.png',40991.8,'Good choise for gaming'),
+		   ('Fridge', 'ourhost.static.com/img/products/999aaef.png',50124.4,'Very big fridge for big family'),
+		   ('Iron', 'ourhost.static.com/img/products/654fa55.png',2034.9,'Regular iron for regular clothes'),
+		   ('Microwave oven', 'ourhost.static.com/img/products/12345ab.png',3000,'900W microwave oven'),
+		   ('WiFi Router', 'ourhost.static.com/img/products/eaa456f.png',2555.5,'Good WPA wireless WiFi router with strong signal'),
+		   ('Happyness', 'ourhost.static.com/img/products/7777777.png',777.77,'Real happyness that you can afford');
---
+++ EShopService/EShopService.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EShopService.Data
+{
+    public class Class1
+    {
+    }
+}
